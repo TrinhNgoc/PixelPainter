de6093a101a2ad0406702d74e0e121a6cb5dfcfe
Jenny <jennyngu@hawaii.edu>
2014-12-06 19:10:02 -1000

    Colors in grid

--- js/PixelPainter.js
+++ js/PixelPainter.js
@@ -3,16 +3,30 @@ $(function() {
     this.artboard = $("<div>");
     this.controls = $("<div>");
 
-    for (var i = 0; i < width -1; i++) {
+    for (var i = 0; i < width; i++) {
       var row = $('<div class="row">')
-      for (var j = 0; j < height -1; j++) {
+      for (var j = 0; j < height; j++) {
         var block = $('<div class="block">')
         $(row).append(block);
       }
      this.artboard.append(row); 
     }
 
-    // return PixelPainter;
+    for (var i = 0; i < 15; i++) {
+      var row = $('<div class="row">')
+      for (var j = 0; j < 9; j++) {
+        var block = $('<div class="block">')
+        $(row).append(block);
+        // Random Color Generator
+        var rgb = [];
+        for (var k = 0; k < 3; k++) {
+          rgb.push(Math.floor(Math.random() * 255))
+        }
+        block.css('background-color', 'rgb(' + rgb.join(',') + ')');
+      }
+     this.controls.append(row); 
+    }
+
   }
 
 
